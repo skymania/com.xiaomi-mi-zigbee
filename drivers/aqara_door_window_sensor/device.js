d8a79f0ba206a1b7072a808b200d78d3488a0bfb
dT <dTNL.Homey@gmail.com>
2017-09-14 21:25:00 +0200

    add printNode log

--- drivers/aqara_door_window_sensor/device.js
+++ drivers/aqara_door_window_sensor/device.js
@@ -9,6 +9,30 @@ const ZigBeeDevice = require('homey-meshdriver').ZigBeeDevice;
 // inClusters: "0000, 0003",
 // outClusters: "0000, 0004",
 // manufacturer: "LUMI", model: "lumi.sensor_magnet.aq2", deviceJoinName: "Xiaomi Aqara Door Sensor"
+/*
+2017-09-14 20:48:16 [log] [ManagerDrivers] [aqara_door_window_sensor] [0] ------------------------------------------
+2017-09-14 20:48:16 [log] [ManagerDrivers] [aqara_door_window_sensor] [0] Node: e827ed2c-c8ec-4cba-9072-97131017da00
+2017-09-14 20:48:16 [log] [ManagerDrivers] [aqara_door_window_sensor] [0] - Battery: false
+2017-09-14 20:48:16 [log] [ManagerDrivers] [aqara_door_window_sensor] [0] - Endpoints: 0
+2017-09-14 20:48:16 [log] [ManagerDrivers] [aqara_door_window_sensor] [0] -- Clusters:
+2017-09-14 20:48:16 [log] [ManagerDrivers] [aqara_door_window_sensor] [0] --- zapp
+2017-09-14 20:48:16 [log] [ManagerDrivers] [aqara_door_window_sensor] [0] --- genBasic
+2017-09-14 20:48:16 [log] [ManagerDrivers] [aqara_door_window_sensor] [0] ---- cid : genBasic
+2017-09-14 20:48:16 [log] [ManagerDrivers] [aqara_door_window_sensor] [0] ---- sid : attrs
+2017-09-14 20:48:16 [log] [ManagerDrivers] [aqara_door_window_sensor] [0] --- genIdentify
+2017-09-14 20:48:16 [log] [ManagerDrivers] [aqara_door_window_sensor] [0] ---- cid : genIdentify
+2017-09-14 20:48:16 [log] [ManagerDrivers] [aqara_door_window_sensor] [0] ---- sid : attrs
+2017-09-14 20:48:16 [log] [ManagerDrivers] [aqara_door_window_sensor] [0] --- genGroups
+2017-09-14 20:48:16 [log] [ManagerDrivers] [aqara_door_window_sensor] [0] ---- cid : genGroups
+2017-09-14 20:48:16 [log] [ManagerDrivers] [aqara_door_window_sensor] [0] ---- sid : attrs
+2017-09-14 20:48:16 [log] [ManagerDrivers] [aqara_door_window_sensor] [0] --- genOnOff
+2017-09-14 20:48:16 [log] [ManagerDrivers] [aqara_door_window_sensor] [0] ---- cid : genOnOff
+2017-09-14 20:48:16 [log] [ManagerDrivers] [aqara_door_window_sensor] [0] ---- sid : attrs
+2017-09-14 20:48:16 [log] [ManagerDrivers] [aqara_door_window_sensor] [0] --- manuSpecificCluster
+2017-09-14 20:48:16 [log] [ManagerDrivers] [aqara_door_window_sensor] [0] ---- cid : manuSpecificCluster
+2017-09-14 20:48:16 [log] [ManagerDrivers] [aqara_door_window_sensor] [0] ---- sid : attrs
+2017-09-14 20:48:16 [log] [ManagerDrivers] [aqara_door_window_sensor] [0] ------------------------------------------
+*/
 
 class AqaraDoorWindowSensor extends ZigBeeDevice {
 	onMeshInit() {
