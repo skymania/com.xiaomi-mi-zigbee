6560950330a486f6239765b3f40047d30e4ee5e7
dT <dTNL.Homey@gmail.com>
2017-10-21 00:59:40 +0200

    printNode update for Xiaomi devices

--- drivers/sens/device.js
+++ drivers/sens/device.js
@@ -9,7 +9,69 @@ const ZigBeeDevice = require('homey-meshdriver').ZigBeeDevice;
 // fingerprint profileId: "0104", deviceId: "0302",
 // inClusters: "0000 (Basic),0001(Power Configuration),0003(Identify),0009(Alarms),0402(Temperature Measurement),0405(Relative Humidity Measurement)"
 
-class XiaomiDoorWindowSensor extends ZigBeeDevice {
+/*
+2017-10-21 00:48:57 [log] [ManagerDrivers] [sens] [0] ------------------------------------------
+2017-10-21 00:48:58 [log] [ManagerDrivers] [sens] [0] Node: 78db4c1a-5cde-4f65-b68c-42ba2832ca3e
+2017-10-21 00:48:58 [log] [ManagerDrivers] [sens] [0] - Battery: false
+2017-10-21 00:48:58 [log] [ManagerDrivers] [sens] [0] - Endpoints: 0
+2017-10-21 00:48:58 [log] [ManagerDrivers] [sens] [0] -- Clusters:
+2017-10-21 00:48:58 [log] [ManagerDrivers] [sens] [0] --- zapp
+2017-10-21 00:48:58 [log] [ManagerDrivers] [sens] [0] --- genBasic
+2017-10-21 00:48:58 [log] [ManagerDrivers] [sens] [0] ---- cid : genBasic
+2017-10-21 00:48:58 [log] [ManagerDrivers] [sens] [0] ---- sid : attrs
+2017-10-21 00:48:58 [log] [ManagerDrivers] [sens] [0] ---- modelId : lumi.sensor_ht
+2017-10-21 00:48:58 [log] [ManagerDrivers] [sens] [0] --- genIdentify
+2017-10-21 00:48:58 [log] [ManagerDrivers] [sens] [0] ---- cid : genIdentify
+2017-10-21 00:48:58 [log] [ManagerDrivers] [sens] [0] ---- sid : attrs
+2017-10-21 00:48:58 [log] [ManagerDrivers] [sens] [0] --- genGroups
+2017-10-21 00:48:58 [log] [ManagerDrivers] [sens] [0] ---- cid : genGroups
+2017-10-21 00:48:58 [log] [ManagerDrivers] [sens] [0] ---- sid : attrs
+2017-10-21 00:48:58 [log] [ManagerDrivers] [sens] [0] --- genScenes
+2017-10-21 00:48:58 [log] [ManagerDrivers] [sens] [0] ---- cid : genScenes
+2017-10-21 00:48:58 [log] [ManagerDrivers] [sens] [0] ---- sid : attrs
+2017-10-21 00:48:58 [log] [ManagerDrivers] [sens] [0] --- genMultistateInput
+2017-10-21 00:48:58 [log] [ManagerDrivers] [sens] [0] ---- cid : genMultistateInput
+2017-10-21 00:48:58 [log] [ManagerDrivers] [sens] [0] ---- sid : attrs
+2017-10-21 00:48:58 [log] [ManagerDrivers] [sens] [0] --- genOta
+2017-10-21 00:48:58 [log] [ManagerDrivers] [sens] [0] ---- cid : genOta
+2017-10-21 00:48:58 [log] [ManagerDrivers] [sens] [0] ---- sid : attrs
+2017-10-21 00:48:58 [log] [ManagerDrivers] [sens] [0] --- manuSpecificCluster
+2017-10-21 00:48:58 [log] [ManagerDrivers] [sens] [0] ---- cid : manuSpecificCluster
+2017-10-21 00:48:58 [log] [ManagerDrivers] [sens] [0] ---- sid : attrs
+2017-10-21 00:48:58 [log] [ManagerDrivers] [sens] [0] - Endpoints: 1
+2017-10-21 00:48:58 [log] [ManagerDrivers] [sens] [0] -- Clusters:
+2017-10-21 00:48:58 [log] [ManagerDrivers] [sens] [0] --- zapp
+2017-10-21 00:48:58 [log] [ManagerDrivers] [sens] [0] --- genIdentify
+2017-10-21 00:48:58 [log] [ManagerDrivers] [sens] [0] ---- cid : genIdentify
+2017-10-21 00:48:58 [log] [ManagerDrivers] [sens] [0] ---- sid : attrs
+2017-10-21 00:48:58 [log] [ManagerDrivers] [sens] [0] --- genGroups
+2017-10-21 00:48:58 [log] [ManagerDrivers] [sens] [0] ---- cid : genGroups
+2017-10-21 00:48:58 [log] [ManagerDrivers] [sens] [0] ---- sid : attrs
+2017-10-21 00:48:58 [log] [ManagerDrivers] [sens] [0] --- genScenes
+2017-10-21 00:48:58 [log] [ManagerDrivers] [sens] [0] ---- cid : genScenes
+2017-10-21 00:48:58 [log] [ManagerDrivers] [sens] [0] ---- sid : attrs
+2017-10-21 00:48:58 [log] [ManagerDrivers] [sens] [0] --- genMultistateInput
+2017-10-21 00:48:58 [log] [ManagerDrivers] [sens] [0] ---- cid : genMultistateInput
+2017-10-21 00:48:58 [log] [ManagerDrivers] [sens] [0] ---- sid : attrs
+2017-10-21 00:48:58 [log] [ManagerDrivers] [sens] [0] - Endpoints: 2
+2017-10-21 00:48:58 [log] [ManagerDrivers] [sens] [0] -- Clusters:
+2017-10-21 00:48:58 [log] [ManagerDrivers] [sens] [0] --- zapp
+2017-10-21 00:48:58 [log] [ManagerDrivers] [sens] [0] --- genIdentify
+2017-10-21 00:48:58 [log] [ManagerDrivers] [sens] [0] ---- cid : genIdentify
+2017-10-21 00:48:58 [log] [ManagerDrivers] [sens] [0] ---- sid : attrs
+2017-10-21 00:48:58 [log] [ManagerDrivers] [sens] [0] --- genGroups
+2017-10-21 00:48:58 [log] [ManagerDrivers] [sens] [0] ---- cid : genGroups
+2017-10-21 00:48:58 [log] [ManagerDrivers] [sens] [0] ---- sid : attrs
+2017-10-21 00:48:58 [log] [ManagerDrivers] [sens] [0] --- genScenes
+2017-10-21 00:48:58 [log] [ManagerDrivers] [sens] [0] ---- cid : genScenes
+2017-10-21 00:48:58 [log] [ManagerDrivers] [sens] [0] ---- sid : attrs
+2017-10-21 00:48:58 [log] [ManagerDrivers] [sens] [0] --- genAnalogInput
+2017-10-21 00:48:58 [log] [ManagerDrivers] [sens] [0] ---- cid : genAnalogInput
+2017-10-21 00:48:58 [log] [ManagerDrivers] [sens] [0] ---- sid : attrs
+2017-10-21 00:48:58 [log] [ManagerDrivers] [sens] [0] ------------------------------------------
+*/
+
+class XiaomiTempSensor extends ZigBeeDevice {
 	onMeshInit() {
 
 		// enable debugging
@@ -18,27 +80,81 @@ class XiaomiDoorWindowSensor extends ZigBeeDevice {
 		// print the node's info to the console
 		this.printNode();
 
-		// Temperature msTemperatureMeasurement measuredValue endpoint 1
-		// this.registerReportListener('msTemperatureMeasurement', 'measuredValue', report => {
-		//	console.log(report);
-		//}, 1);
-		// Humidity msRelativeHumidity measuredValue endpoint 1
-		//this.registerReportListener('msRelativeHumidity', 'measuredValue', report => {
-		//	console.log(report);
-		//}, 1);
+		// enable debugging
+		// this.enableDebug();
 
-		if (this.node) {
+		// print the node's info to the console
+		// this.printNode();
 
-			// Listen to all the commands that come in
-			this.node.on('report', report => {
-				console.log('Command received');
-				console.log(report);
-				console.log(report.endpoint);
-				console.log(report.attr);
-				console.log(report.value);
+		// Temperature Cluster (0x0402)
+		this.log('Initializing Temperature (0x0402) Cluster');
+		// Min report interval in seconds (must be greater than 1)
+		this.minIntTemp = this.getSetting('minIntTemp') || 30;
+		// Max report interval in seconds (must be zero or greater than 60 and greater than min report interval)
+		this.maxIntTemp = this.getSetting('maxIntTemp') || 120;
+		// Report change value, if value changed more than this value send a report
+		this.repChangeTemp = this.getSetting('repChangeTemp') || 50; // note: 1 = 0.01 [°C]
+
+		// Register the AttributeReportListener
+		this.registerAttrReportListener(
+				'msTemperatureMeasurement', // Cluster
+				'measuredValue', // Attr
+				this.minIntTemp,
+				this.maxIntTemp,
+				this.repChangeTemp,
+				this.onTemperatureReport.bind(this), // Callback with value
+				0) // The endpoint index
+			.then(() => {
+				// Registering attr reporting succeeded
+				this.log('registered attr report listener');
+			})
+			.catch(err => {
+				// Registering attr reporting failed
+				this.error('failed to register attr report listener', err);
 			});
-		}
+
+		// Humidity Cluster (0x0405)
+		this.log('Initializing Humidity (0x0405) Cluster');
+		// The minimal reporting interval in seconds (e.g. 10 (seconds)):
+		this.minIntHum = this.getSetting('minIntHum') || 60;
+		// The maximal reporting interval in seconds (e.g. 300 (seconds))
+		this.maxIntHum = this.getSetting('maxIntHum') || 180;
+		// Reportable change; the attribute should report its value when the value is changed more than this setting.
+		this.repChangeHum = this.getSetting('repChangeHum') || 100; // note: 1 = 0.01 [%]
+
+		// Register the AttributeReportListener
+		this.registerAttrReportListener(
+				'msRelativeHumidity', // Cluster
+				'measuredValue', // Attr
+				this.minIntHum,
+				this.maxIntHum,
+				this.repChangeHum,
+				this.onHumidityReport.bind(this), // Callback with value
+				0) // The endpoint index
+			.then(() => {
+				// Registering attr reporting succeeded
+				this.log('registered attr report listener');
+			})
+			.catch(err => {
+				// Registering attr reporting failed
+				this.error('failed to register attr report listener', err);
+			});
+
+		this.log('Fully initialized');
+
 	}
+	onTemperatureReport(value) {
+		let parsedValue = Math.round((value / 100) * 10) / 10;
+		this.log('msTemperatureMeasurement - measuredValue', parsedValue);
+		this.setCapabilityValue('measure_temperature', parsedValue);
+	}
+
+	onHumidityReport(value) {
+		let parsedValue = Math.round((value / 100) * 10) / 10;
+		this.log('msRelativeHumidity - measuredValue', parsedValue);
+		this.setCapabilityValue('measure_humidity', parsedValue);
+	}
+
 }
 
-module.exports = XiaomiDoorWindowSensor;
+module.exports = XiaomiTempSensor;
--- drivers/sensor_magnet/device.js
+++ drivers/sensor_magnet/device.js
@@ -11,6 +11,39 @@ const ZigBeeDevice = require('homey-meshdriver').ZigBeeDevice;
 // outClusters: "0000(Basic), 0004(Groups), 0003(Identify), 0006(On/Off), 0008(Level Control), 0005(Scenes)",
 // manufacturer: "LUMI", model: "lumi.sensor_magnet", deviceJoinName: "Xiaomi Door Sensor"
 
+/*
+2017-10-21 00:55:34 [log] [ManagerDrivers] [sensor_magnet] [0] Node: 2a3902d3-988a-4ae5-adea-6e0d7c85ec5e
+2017-10-21 00:55:34 [log] [ManagerDrivers] [sensor_magnet] [0] - Battery: false
+2017-10-21 00:55:34 [log] [ManagerDrivers] [sensor_magnet] [0] - Endpoints: 0
+2017-10-21 00:55:34 [log] [ManagerDrivers] [sensor_magnet] [0] -- Clusters:
+2017-10-21 00:55:34 [log] [ManagerDrivers] [sensor_magnet] [0] --- zapp
+2017-10-21 00:55:34 [log] [ManagerDrivers] [sensor_magnet] [0] --- genBasic
+2017-10-21 00:55:34 [log] [ManagerDrivers] [sensor_magnet] [0] ---- cid : genBasic
+2017-10-21 00:55:34 [log] [ManagerDrivers] [sensor_magnet] [0] ---- sid : attrs
+2017-10-21 00:55:34 [log] [ManagerDrivers] [sensor_magnet] [0] --- genIdentify
+2017-10-21 00:55:34 [log] [ManagerDrivers] [sensor_magnet] [0] ---- cid : genIdentify
+2017-10-21 00:55:34 [log] [ManagerDrivers] [sensor_magnet] [0] ---- sid : attrs
+2017-10-21 00:55:34 [log] [ManagerDrivers] [sensor_magnet] [0] --- genGroups
+2017-10-21 00:55:34 [log] [ManagerDrivers] [sensor_magnet] [0] ---- cid : genGroups
+2017-10-21 00:55:34 [log] [ManagerDrivers] [sensor_magnet] [0] ---- sid : attrs
+2017-10-21 00:55:34 [log] [ManagerDrivers] [sensor_magnet] [0] --- genScenes
+2017-10-21 00:55:34 [log] [ManagerDrivers] [sensor_magnet] [0] ---- cid : genScenes
+2017-10-21 00:55:34 [log] [ManagerDrivers] [sensor_magnet] [0] ---- sid : attrs
+2017-10-21 00:55:34 [log] [ManagerDrivers] [sensor_magnet] [0] --- genOnOff
+2017-10-21 00:55:34 [log] [ManagerDrivers] [sensor_magnet] [0] ---- cid : genOnOff
+2017-10-21 00:55:34 [log] [ManagerDrivers] [sensor_magnet] [0] ---- sid : attrs
+2017-10-21 00:55:34 [log] [ManagerDrivers] [sensor_magnet] [0] --- genLevelCtrl
+2017-10-21 00:55:34 [log] [ManagerDrivers] [sensor_magnet] [0] ---- cid : genLevelCtrl
+2017-10-21 00:55:34 [log] [ManagerDrivers] [sensor_magnet] [0] ---- sid : attrs
+2017-10-21 00:55:34 [log] [ManagerDrivers] [sensor_magnet] [0] --- genOta
+2017-10-21 00:55:34 [log] [ManagerDrivers] [sensor_magnet] [0] ---- cid : genOta
+2017-10-21 00:55:34 [log] [ManagerDrivers] [sensor_magnet] [0] ---- sid : attrs
+2017-10-21 00:55:34 [log] [ManagerDrivers] [sensor_magnet] [0] --- manuSpecificCluster
+2017-10-21 00:55:34 [log] [ManagerDrivers] [sensor_magnet] [0] ---- cid : manuSpecificCluster
+2017-10-21 00:55:34 [log] [ManagerDrivers] [sensor_magnet] [0] ---- sid : attrs
+2017-10-21 00:55:34 [log] [ManagerDrivers] [sensor_magnet] [0] ------------------------------------------
+*/
+
 class XiaomiDoorWindowSensor extends ZigBeeDevice {
 	onMeshInit() {
 
