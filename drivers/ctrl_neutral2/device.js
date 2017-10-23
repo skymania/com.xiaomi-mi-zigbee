14f18a0af8ca984aecd61c5f11e190adb52a6159
sprut666666 <sprut.1@me.com>
2017-10-23 23:37:39 +0300

    add node

--- drivers/ctrl_neutral2/device.js
+++ drivers/ctrl_neutral2/device.js
@@ -1,3 +1,168 @@
+/*
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] Node: d4560311-d095-41b1-8ae8-fc57398fe6a0
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] - Battery: false
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] - Endpoints: 0
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] -- Clusters:
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] --- zapp
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] --- genBasic
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- 65281 : den o !�
+                                                                                    ( !!&
+!N��#�!
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- cid : genBasic
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- sid : attrs
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- zclVersion : 0
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- appVersion : 17
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- stackVersion : 2
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- hwVersion : 38
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- manufacturerName : LUMI
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- modelId : lumi.ctrl_neutral2
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- dateCode : 08-31-2016
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- powerSource : 4
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- locationDesc :
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] --- genPowerCfg
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- cid : genPowerCfg
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- sid : attrs
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- mainsVoltage : 0
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- mainsAlarmMask : 0
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- batteryVoltage : 0
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] --- genDeviceTempCfg
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- cid : genDeviceTempCfg
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- sid : attrs
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- currentTemperature : 31
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- lowTempThres : 55
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- highTempThres : 60
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] --- genIdentify
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- cid : genIdentify
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- sid : attrs
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- identifyTime : 0
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] --- genTime
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- cid : genTime
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- sid : attrs
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] --- genOta
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- cid : genOta
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- sid : attrs
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] - Endpoints: 1
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] -- Clusters:
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] --- zapp
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] --- genGroups
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- cid : genGroups
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- sid : attrs
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- nameSupport : 128
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] --- genScenes
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- cid : genScenes
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- sid : attrs
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- count : 0
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- currentScene : 0
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- currentGroup : 0
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- sceneValid : 0
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- nameSupport : 128
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] --- genOnOff
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- 61440 : 117440551
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- cid : genOnOff
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- sid : attrs
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- onOff : 0
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] --- genBinaryOutput
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- cid : genBinaryOutput
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- sid : attrs
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- activeText : ON
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- description : CHANNEL1
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- inactiveText : OFF
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- minimumOffTime : 500000
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- minimumOnTime : 500000
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- outOfService : 0
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- presentValue : 0
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- statusFlags : 0
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- applicationType : 67109376
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] - Endpoints: 2
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] -- Clusters:
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] --- zapp
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] --- genGroups
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- cid : genGroups
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- sid : attrs
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- nameSupport : 128
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] --- genScenes
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- cid : genScenes
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- sid : attrs
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- count : 0
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- currentScene : 0
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- currentGroup : 0
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- sceneValid : 0
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- nameSupport : 128
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] --- genOnOff
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- 61440 : 117440556
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- cid : genOnOff
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- sid : attrs
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- onOff : 0
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] --- genBinaryOutput
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- cid : genBinaryOutput
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- sid : attrs
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- activeText : ON
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- description : CHANNEL2
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- inactiveText : OFF
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- minimumOffTime : 500000
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- minimumOnTime : 500000
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- outOfService : 0
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- presentValue : 0
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- statusFlags : 0
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- applicationType : 67109377
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] - Endpoints: 3
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] -- Clusters:
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] --- zapp
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] --- genOnOff
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- cid : genOnOff
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- sid : attrs
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- onOff : 1
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] --- genMultistateInput
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- cid : genMultistateInput
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- sid : attrs
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- numberOfStates : 4
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- outOfService : 0
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- presentValue : 0
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- statusFlags : 0
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] - Endpoints: 4
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] -- Clusters:
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] --- zapp
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] --- genOnOff
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- cid : genOnOff
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- sid : attrs
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- onOff : 1
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] --- genMultistateInput
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- cid : genMultistateInput
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- sid : attrs
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- numberOfStates : 4
+2017-10-23 23:01:17 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- outOfService : 0
+2017-10-23 23:01:18 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- presentValue : 0
+2017-10-23 23:01:18 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- statusFlags : 0
+2017-10-23 23:01:18 [log] [ManagerDrivers] [ctrl_neutral2] [0] - Endpoints: 5
+2017-10-23 23:01:18 [log] [ManagerDrivers] [ctrl_neutral2] [0] -- Clusters:
+2017-10-23 23:01:18 [log] [ManagerDrivers] [ctrl_neutral2] [0] --- zapp
+2017-10-23 23:01:18 [log] [ManagerDrivers] [ctrl_neutral2] [0] --- genOnOff
+2017-10-23 23:01:18 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- cid : genOnOff
+2017-10-23 23:01:18 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- sid : attrs
+2017-10-23 23:01:18 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- onOff : 0
+2017-10-23 23:01:18 [log] [ManagerDrivers] [ctrl_neutral2] [0] --- genMultistateInput
+2017-10-23 23:01:18 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- cid : genMultistateInput
+2017-10-23 23:01:18 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- sid : attrs
+2017-10-23 23:01:18 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- numberOfStates : 4
+2017-10-23 23:01:18 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- outOfService : 0
+2017-10-23 23:01:18 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- presentValue : 0
+2017-10-23 23:01:18 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- statusFlags : 0
+2017-10-23 23:01:18 [log] [ManagerDrivers] [ctrl_neutral2] [0] - Endpoints: 6
+2017-10-23 23:01:18 [log] [ManagerDrivers] [ctrl_neutral2] [0] -- Clusters:
+2017-10-23 23:01:18 [log] [ManagerDrivers] [ctrl_neutral2] [0] --- zapp
+2017-10-23 23:01:18 [log] [ManagerDrivers] [ctrl_neutral2] [0] --- genAnalogInput
+2017-10-23 23:01:18 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- cid : genAnalogInput
+2017-10-23 23:01:18 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- sid : attrs
+2017-10-23 23:01:18 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- description : POWER
+2017-10-23 23:01:18 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- maxPresentValue : 1600
+2017-10-23 23:01:18 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- minPresentValue : 0
+2017-10-23 23:01:18 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- outOfService : 0
+2017-10-23 23:01:18 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- presentValue : 0
+2017-10-23 23:01:18 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- resolution : 1
+2017-10-23 23:01:18 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- statusFlags : 0
+2017-10-23 23:01:18 [log] [ManagerDrivers] [ctrl_neutral2] [0] ---- applicationType : 590336
+*/
+
 'use strict';
 
 const Homey = require('homey');
