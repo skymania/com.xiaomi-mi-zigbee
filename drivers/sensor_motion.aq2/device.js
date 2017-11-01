64d13bda35fd017cf9fe11d1657be797e56aca1c
Robin Bolscher <robinbolscher@gmail.com>
2017-11-01 21:54:28 +0100

    Clean up some drivers

--- drivers/sensor_motion.aq2/device.js
+++ drivers/sensor_motion.aq2/device.js
@@ -1,8 +1,41 @@
 'use strict';
 
-const Homey = require('homey');
-
 const ZigBeeDevice = require('homey-meshdriver').ZigBeeDevice;
+
+class AqaraHumanBodySensor extends ZigBeeDevice {
+
+	onMeshInit() {
+
+		// Register attribute listener for occupancy
+		this.registerAttrReportListener('msOccupancySensing', 'occupancy', 1, 3600, 1,
+			this.onOccupancyReport.bind(this));
+
+		// Register attribute listener for illuminance measurements
+		this.registerAttrReportListener('msIlluminanceMeasurement', 'measuredValue', 1, 3600, 20,
+			this.onIlluminanceReport.bind(this));
+	}
+
+	onOccupancyReport(value) {
+		this.log('alarm_motion', value === 1);
+
+		// Set and clear motion timeout
+		clearTimeout(this.motionTimeout);
+		this.motionTimeout = setTimeout(() => {
+			this.log('manual alarm_motion reset');
+			this.setCapabilityValue('alarm_motion', false);
+		}, (this.getSetting('alarm_motion_reset_window') || 300) * 1000);
+
+		// Update capability value
+		this.setCapabilityValue('alarm_motion', value === 1);
+	}
+
+	onIlluminanceReport(value) {
+		this.log('measure_luminance', value);
+		this.setCapabilityValue('measure_luminance', value);
+	}
+}
+
+module.exports = AqaraHumanBodySensor;
 
 // RTCGQ11LM_sensor_motion
 // fingerprint profileId: "0104", deviceId: "0104",
@@ -11,107 +44,29 @@ const ZigBeeDevice = require('homey-meshdriver').ZigBeeDevice;
 // manufacturer: "LUMI", model: "lumi.sensor_motion.aq2", deviceJoinName: "Xiaomi Motion"
 
 /*
-2017-10-20 23:14:47 [log] [ManagerDrivers] [sensor_motion.aq2] [0] ZigBeeDevice has been inited
-2017-10-20 23:14:47 [log] [ManagerDrivers] [sensor_motion.aq2] [0] ------------------------------------------
-2017-10-20 23:14:47 [log] [ManagerDrivers] [sensor_motion.aq2] [0] Node: 363f731f-57fe-4a43-96fe-0513ae78a907
-2017-10-20 23:14:47 [log] [ManagerDrivers] [sensor_motion.aq2] [0] - Battery: false
-2017-10-20 23:14:47 [log] [ManagerDrivers] [sensor_motion.aq2] [0] - Endpoints: 0
-2017-10-20 23:14:47 [log] [ManagerDrivers] [sensor_motion.aq2] [0] -- Clusters:
-2017-10-20 23:14:48 [log] [ManagerDrivers] [sensor_motion.aq2] [0] --- zapp
-2017-10-20 23:14:48 [log] [ManagerDrivers] [sensor_motion.aq2] [0] --- genBasic
-2017-10-20 23:14:48 [log] [ManagerDrivers] [sensor_motion.aq2] [0] ---- cid : genBasic
-2017-10-20 23:14:48 [log] [ManagerDrivers] [sensor_motion.aq2] [0] ---- sid : attrs
-2017-10-20 23:14:48 [log] [ManagerDrivers] [sensor_motion.aq2] [0] --- genOta
-2017-10-20 23:14:48 [log] [ManagerDrivers] [sensor_motion.aq2] [0] ---- cid : genOta
-2017-10-20 23:14:48 [log] [ManagerDrivers] [sensor_motion.aq2] [0] ---- sid : attrs
-2017-10-20 23:14:48 [log] [ManagerDrivers] [sensor_motion.aq2] [0] --- msIlluminanceMeasurement
-2017-10-20 23:14:48 [log] [ManagerDrivers] [sensor_motion.aq2] [0] ---- cid : msIlluminanceMeasurement
-2017-10-20 23:14:48 [log] [ManagerDrivers] [sensor_motion.aq2] [0] ---- sid : attrs
-2017-10-20 23:14:48 [log] [ManagerDrivers] [sensor_motion.aq2] [0] --- msOccupancySensing
-2017-10-20 23:14:48 [log] [ManagerDrivers] [sensor_motion.aq2] [0] ---- cid : msOccupancySensing
-2017-10-20 23:14:48 [log] [ManagerDrivers] [sensor_motion.aq2] [0] ---- sid : attrs
-2017-10-20 23:14:48 [log] [ManagerDrivers] [sensor_motion.aq2] [0] --- manuSpecificCluster
-2017-10-20 23:14:48 [log] [ManagerDrivers] [sensor_motion.aq2] [0] ---- cid : manuSpecificCluster
-2017-10-20 23:14:48 [log] [ManagerDrivers] [sensor_motion.aq2] [0] ---- sid : attrs
-2017-10-20 23:14:48 [log] [ManagerDrivers] [sensor_motion.aq2] [0] ------------------------------------------
+2017-09-14 20:29:21 [log] [ManagerDrivers] [aqara_human_body_sensor] [0] ZigBeeDevice has been inited
+2017-09-14 20:29:21 [log] [ManagerDrivers] [aqara_human_body_sensor] [0] ------------------------------------------
+2017-09-14 20:29:21 [log] [ManagerDrivers] [aqara_human_body_sensor] [0] Node: 52c927a0-98e3-4b1b-8074-2da221c04522
+2017-09-14 20:29:21 [log] [ManagerDrivers] [aqara_human_body_sensor] [0] - Battery: false
+2017-09-14 20:29:21 [log] [ManagerDrivers] [aqara_human_body_sensor] [0] - Endpoints: 0
+2017-09-14 20:29:21 [log] [ManagerDrivers] [aqara_human_body_sensor] [0] -- Clusters:
+2017-09-14 20:29:21 [log] [ManagerDrivers] [aqara_human_body_sensor] [0] --- zapp
+2017-09-14 20:29:21 [log] [ManagerDrivers] [aqara_human_body_sensor] [0] --- genBasic
+2017-09-14 20:29:21 [log] [ManagerDrivers] [aqara_human_body_sensor] [0] ---- cid : genBasic
+2017-09-14 20:29:21 [log] [ManagerDrivers] [aqara_human_body_sensor] [0] ---- sid : attrs
+2017-09-14 20:29:21 [log] [ManagerDrivers] [aqara_human_body_sensor] [0] ---- manufacturerName : LUMI
+2017-09-14 20:29:21 [log] [ManagerDrivers] [aqara_human_body_sensor] [0] ---- modelId : lumi.sensor_motion.aq2
+2017-09-14 20:29:21 [log] [ManagerDrivers] [aqara_human_body_sensor] [0] --- genOta
+2017-09-14 20:29:21 [log] [ManagerDrivers] [aqara_human_body_sensor] [0] ---- cid : genOta
+2017-09-14 20:29:21 [log] [ManagerDrivers] [aqara_human_body_sensor] [0] ---- sid : attrs
+2017-09-14 20:29:21 [log] [ManagerDrivers] [aqara_human_body_sensor] [0] --- msIlluminanceMeasurement
+2017-09-14 20:29:21 [log] [ManagerDrivers] [aqara_human_body_sensor] [0] ---- cid : msIlluminanceMeasurement
+2017-09-14 20:29:21 [log] [ManagerDrivers] [aqara_human_body_sensor] [0] ---- sid : attrs
+2017-09-14 20:29:21 [log] [ManagerDrivers] [aqara_human_body_sensor] [0] --- msOccupancySensing
+2017-09-14 20:29:21 [log] [ManagerDrivers] [aqara_human_body_sensor] [0] ---- cid : msOccupancySensing
+2017-09-14 20:29:21 [log] [ManagerDrivers] [aqara_human_body_sensor] [0] ---- sid : attrs
+2017-09-14 20:29:21 [log] [ManagerDrivers] [aqara_human_body_sensor] [0] --- manuSpecificCluster
+2017-09-14 20:29:21 [log] [ManagerDrivers] [aqara_human_body_sensor] [0] ---- cid : manuSpecificCluster
+2017-09-14 20:29:21 [log] [ManagerDrivers] [aqara_human_body_sensor] [0] ---- sid : attrs
+2017-09-14 20:29:21 [log] [ManagerDrivers] [aqara_human_body_sensor] [0] ------------------------------------------
 */
-
-class AqaraHumanBodySensor extends ZigBeeDevice {
-	onMeshInit() {
-
-		// enable debugging
-		this.enableDebug();
-
-		// print the node's info to the console
-		this.printNode();
-
-		// Occupancy Cluster
-		// Register alarm_motion capability
-		this.log('Initializing Occupancy (0x0401) Cluster');
-		// The minimal reporting interval in seconds (e.g. 10 (seconds)):
-		this.minIntMot = this.getSetting('minIntMot') || 1;
-		// The maximal reporting interval in seconds (e.g. 300 (seconds))
-		this.maxIntMot = this.getSetting('maxIntMot') || 600; // 600 = 60 seconds
-
-		// The maximal reporting interval in seconds (e.g. 300 (seconds))
-		this.IntCanMot = this.getSetting('IntCanMot') || 60;
-
-		// Register the AttributeReportListener
-		this.registerAttrReportListener(
-				'msOccupancySensing', // Cluster
-				'occupancy', // Attr
-				this.minIntMot,
-				this.maxIntMot,
-				1,
-				this.onMotionReport.bind(this), // Callback with value
-				0) // The endpoint index
-			.then(() => {
-				// Registering attr reporting succeeded
-				this.log('registered attr report listener');
-			})
-			.catch(err => {
-				// Registering attr reporting failed
-				this.error('failed to register attr report listener', err);
-			});
-
-		// Illuminance Cluster
-		// Occupancy Cluster
-		this.log('Initializing Illuminance Cluster');
-		// The minimal reporting interval in seconds (e.g. 10 (seconds)):
-		this.minIntLum = this.getSetting('minIntLum') || 1;
-		// The maximal reporting interval in seconds (e.g. 300 (seconds))
-		this.maxIntLum = this.getSetting('maxIntLum') || 3000; // 3000 = 300 seconds
-		// The maximal reporting interval in seconds (e.g. 300 (seconds))
-		this.repChangeLum = this.getSetting('repChangeLum') || 10;
-
-		// Register the AttributeReportListener
-		this.registerAttrReportListener(
-				'msIlluminanceMeasurement', // Cluster
-				'measuredValue', // Attr
-				this.minIntMot,
-				this.maxIntMot,
-				this.repChangeLum,
-				this.onLuminanceReport.bind(this), // Callback with value
-				0) // The endpoint index
-			.then(() => {
-				// Registering attr reporting succeeded
-				this.log('registered attr report listener');
-			})
-			.catch(err => {
-				// Registering attr reporting failed
-				this.error('failed to register attr report listener', err);
-			});
-
-	}
-	onMotionReport(value) {
-		this.log('msOccupancySensing - occupancy', value === 1);
-		this.setCapabilityValue('alarm_motion', value === 1);
-	}
-	onLuminanceReport(value) {
-		const luminanceValue = value; //Math.round(Math.pow(10, (value - 1) / 10000));
-		this.log('msIlluminanceMeasurement - measuredValue', value, luminanceValue);
-		this.setCapabilityValue('measure_luminance', luminanceValue);
-	}
-}
-
-module.exports = AqaraHumanBodySensor;
--- drivers/weather/device.js
+++ drivers/weather/device.js
@@ -1,8 +1,55 @@
 'use strict';
 
-const Homey = require('homey');
-
 const ZigBeeDevice = require('homey-meshdriver').ZigBeeDevice;
+
+class AqaraWeatherSensor extends ZigBeeDevice {
+	onMeshInit() {
+
+		const minIntTemp = this.getSetting('minIntTemp') || 60;
+		const maxIntTemp = this.getSetting('maxIntTemp') || 3600;
+		const repChangeTemp = this.getSetting('repChangeTemp') || 50; // note: 1 = 0.01 [°C]
+
+		// Register the AttributeReportListener
+		this.registerAttrReportListener('msTemperatureMeasurement', 'measuredValue', minIntTemp, maxIntTemp, repChangeTemp,
+			this.onTemperatureReport.bind(this));
+
+		const minIntHum = this.getSetting('minIntHum') || 60;
+		const maxIntHum = this.getSetting('maxIntHum') || 3600;
+		const repChangeHum = this.getSetting('repChangeHum') || 100; // note: 1 = 0.01 [%]
+
+		// Register the AttributeReportListener
+		this.registerAttrReportListener('msRelativeHumidity', 'measuredValue', minIntHum, maxIntHum, repChangeHum,
+			this.onHumidityReport.bind(this));
+
+		const minIntPres = this.getSetting('minIntPres') || 60;
+		const maxIntPres = this.getSetting('maxIntPres') || 3600;
+		const repChangePres = this.getSetting('repChangePres') || 100; // note: 1 = 0.01 [%]
+
+		// Register the AttributeReportListener
+		this.registerAttrReportListener('msPressureMeasurement', 'measuredValue', minIntPres, maxIntPres, repChangePres,
+			this.onPressureReport.bind(this));
+	}
+
+	onTemperatureReport(value) {
+		const parsedValue = Math.round((value / 100) * 10) / 10;
+		this.log('measure_temperature', parsedValue);
+		this.setCapabilityValue('measure_temperature', parsedValue);
+	}
+
+	onHumidityReport(value) {
+		const parsedValue = Math.round((value / 100) * 10) / 10;
+		this.log('measure_humidity', parsedValue);
+		this.setCapabilityValue('measure_humidity', parsedValue);
+	}
+
+	onPressureReport(value) {
+		const parsedValue = Math.round((value / 100) * 10) / 10;
+		this.log('measure_pressure', parsedValue);
+		this.setCapabilityValue('measure_pressure', parsedValue);
+	}
+}
+
+module.exports = AqaraWeatherSensor;
 
 // WSDCGQ11LM_weather
 // https://github.com/bspranger/Xiaomi/blob/master/devicetypes/a4refillpad/xiaomi-aqara-temperature-humidity-sensor.src/xiaomi-aqara-temperature-humidity-sensor.groovy
@@ -11,162 +58,30 @@ const ZigBeeDevice = require('homey-meshdriver').ZigBeeDevice;
 // manufacturer: "LUMI", model: "lumi.weather", deviceJoinName: "Xiaomi Aqara Temp Sensor"
 
 /*
-2017-10-20 22:48:42 [log] [ManagerDrivers] [weather] [0] ZigBeeDevice has been inited
-2017-10-20 22:48:42 [log] [ManagerDrivers] [weather] [0] ------------------------------------------
-2017-10-20 22:48:42 [log] [ManagerDrivers] [weather] [0] Node: 00881eb0-f819-44c5-ade7-87b56d3f7a14
-2017-10-20 22:48:42 [log] [ManagerDrivers] [weather] [0] - Battery: false
-2017-10-20 22:48:42 [log] [ManagerDrivers] [weather] [0] - Endpoints: 0
-2017-10-20 22:48:42 [log] [ManagerDrivers] [weather] [0] -- Clusters:
-2017-10-20 22:48:42 [log] [ManagerDrivers] [weather] [0] --- zapp
-2017-10-20 22:48:42 [log] [ManagerDrivers] [weather] [0] --- genBasic
-2017-10-20 22:48:42 [log] [ManagerDrivers] [weather] [0] ---- cid : genBasic
-2017-10-20 22:48:42 [log] [ManagerDrivers] [weather] [0] ---- sid : attrs
-2017-10-20 22:48:42 [log] [ManagerDrivers] [weather] [0] ---- manufacturerName : LUMI
-2017-10-20 22:48:42 [log] [ManagerDrivers] [weather] [0] ---- modelId : lumi.weather
-2017-10-20 22:48:42 [log] [ManagerDrivers] [weather] [0] --- genIdentify
-2017-10-20 22:48:42 [log] [ManagerDrivers] [weather] [0] ---- cid : genIdentify
-2017-10-20 22:48:42 [log] [ManagerDrivers] [weather] [0] ---- sid : attrs
-2017-10-20 22:48:42 [log] [ManagerDrivers] [weather] [0] --- genGroups
-2017-10-20 22:48:42 [log] [ManagerDrivers] [weather] [0] ---- cid : genGroups
-2017-10-20 22:48:42 [log] [ManagerDrivers] [weather] [0] ---- sid : attrs
-2017-10-20 22:48:42 [log] [ManagerDrivers] [weather] [0] --- msTemperatureMeasurement
-2017-10-20 22:48:42 [log] [ManagerDrivers] [weather] [0] ---- cid : msTemperatureMeasurement
-2017-10-20 22:48:42 [log] [ManagerDrivers] [weather] [0] ---- sid : attrs
-2017-10-20 22:48:42 [log] [ManagerDrivers] [weather] [0] ---- measuredValue : 2613
-2017-10-20 22:48:43 [log] [ManagerDrivers] [weather] [0] --- msPressureMeasurement
-2017-10-20 22:48:43 [log] [ManagerDrivers] [weather] [0] ---- 16 : 10101
-2017-10-20 22:48:43 [log] [ManagerDrivers] [weather] [0] ---- 20 : -1
-2017-10-20 22:48:44 [log] [ManagerDrivers] [weather] [0] ---- cid : msPressureMeasurement
-2017-10-20 22:48:44 [log] [ManagerDrivers] [weather] [0] ---- sid : attrs
-2017-10-20 22:48:44 [log] [ManagerDrivers] [weather] [0] ---- measuredValue : 1010
-2017-10-20 22:48:44 [log] [ManagerDrivers] [weather] [0] --- msRelativeHumidity
-2017-10-20 22:48:44 [log] [ManagerDrivers] [weather] [0] ---- cid : msRelativeHumidity
-2017-10-20 22:48:45 [log] [ManagerDrivers] [weather] [0] ---- sid : attrs
-2017-10-20 22:48:45 [log] [ManagerDrivers] [weather] [0] ---- measuredValue : 5356
-2017-10-20 22:48:45 [log] [ManagerDrivers] [weather] [0] --- manuSpecificCluster
-2017-10-20 22:48:45 [log] [ManagerDrivers] [weather] [0] ---- cid : manuSpecificCluster
-2017-10-20 22:48:45 [log] [ManagerDrivers] [weather] [0] ---- sid : attrs
-2017-10-20 22:48:45 [log] [ManagerDrivers] [weather] [0] ------------------------------------------
+2017-09-09 18:54:06 [log] [ManagerDrivers] [temperature_humidity_pressure_sensor] [0] - Battery: false
+2017-09-09 18:54:06 [log] [ManagerDrivers] [temperature_humidity_pressure_sensor] [0] - Endpoints: 0
+2017-09-09 18:54:06 [log] [ManagerDrivers] [temperature_humidity_pressure_sensor] [0] -- Clusters:
+2017-09-09 18:54:06 [log] [ManagerDrivers] [temperature_humidity_pressure_sensor] [0] --- zapp
+2017-09-09 18:54:06 [log] [ManagerDrivers] [temperature_humidity_pressure_sensor] [0] --- genBasic
+2017-09-09 18:54:06 [log] [ManagerDrivers] [temperature_humidity_pressure_sensor] [0] ---- cid : genBasic
+2017-09-09 18:54:06 [log] [ManagerDrivers] [temperature_humidity_pressure_sensor] [0] ---- sid : attrs
+2017-09-09 18:54:06 [log] [ManagerDrivers] [temperature_humidity_pressure_sensor] [0] --- genIdentify
+2017-09-09 18:54:06 [log] [ManagerDrivers] [temperature_humidity_pressure_sensor] [0] ---- cid : genIdentify
+2017-09-09 18:54:06 [log] [ManagerDrivers] [temperature_humidity_pressure_sensor] [0] ---- sid : attrs
+2017-09-09 18:54:07 [log] [ManagerDrivers] [temperature_humidity_pressure_sensor] [0] --- genGroups
+2017-09-09 18:54:07 [log] [ManagerDrivers] [temperature_humidity_pressure_sensor] [0] ---- cid : genGroups
+2017-09-09 18:54:07 [log] [ManagerDrivers] [temperature_humidity_pressure_sensor] [0] ---- sid : attrs
+2017-09-09 18:54:07 [log] [ManagerDrivers] [temperature_humidity_pressure_sensor] [0] --- msTemperatureMeasurement
+2017-09-09 18:54:07 [log] [ManagerDrivers] [temperature_humidity_pressure_sensor] [0] ---- cid : msTemperatureMeasurement
+2017-09-09 18:54:07 [log] [ManagerDrivers] [temperature_humidity_pressure_sensor] [0] ---- sid : attrs
+2017-09-09 18:54:07 [log] [ManagerDrivers] [temperature_humidity_pressure_sensor] [0] --- msPressureMeasurement
+2017-09-09 18:54:07 [log] [ManagerDrivers] [temperature_humidity_pressure_sensor] [0] ---- cid : msPressureMeasurement
+2017-09-09 18:54:07 [log] [ManagerDrivers] [temperature_humidity_pressure_sensor] [0] ---- sid : attrs
+2017-09-09 18:54:07 [log] [ManagerDrivers] [temperature_humidity_pressure_sensor] [0] --- msRelativeHumidity
+2017-09-09 18:54:07 [log] [ManagerDrivers] [temperature_humidity_pressure_sensor] [0] ---- cid : msRelativeHumidity
+2017-09-09 18:54:07 [log] [ManagerDrivers] [temperature_humidity_pressure_sensor] [0] ---- sid : attrs
+2017-09-09 18:54:07 [log] [ManagerDrivers] [temperature_humidity_pressure_sensor] [0] --- manuSpecificCluster
+2017-09-09 18:54:07 [log] [ManagerDrivers] [temperature_humidity_pressure_sensor] [0] ---- cid : manuSpecificCluster
+2017-09-09 18:54:07 [log] [ManagerDrivers] [temperature_humidity_pressure_sensor] [0] ---- sid : attrs
+2017-09-09 18:54:07 [log] [ManagerDrivers] [temperature_humidity_pressure_sensor] [0] ------------------------------------------
 */
-
-class AqaraWeatherSensor extends ZigBeeDevice {
-	onMeshInit() {
-
-		// enable debugging
-		// this.enableDebug();
-
-		// print the node's info to the console
-		// this.printNode();
-
-		// Temperature Cluster (0x0402)
-		this.log('Initializing Temperature (0x0402) Cluster');
-		// Min report interval in seconds (must be greater than 1)
-		this.minIntTemp = this.getSetting('minIntTemp') || 30;
-		// Max report interval in seconds (must be zero or greater than 60 and greater than min report interval)
-		this.maxIntTemp = this.getSetting('maxIntTemp') || 120;
-		// Report change value, if value changed more than this value send a report
-		this.repChangeTemp = this.getSetting('repChangeTemp') || 50; // note: 1 = 0.01 [°C]
-
-		// Register the AttributeReportListener
-		this.registerAttrReportListener(
-				'msTemperatureMeasurement', // Cluster
-				'measuredValue', // Attr
-				this.minIntTemp,
-				this.maxIntTemp,
-				this.repChangeTemp,
-				this.onTemperatureReport.bind(this), // Callback with value
-				0) // The endpoint index
-			.then(() => {
-				// Registering attr reporting succeeded
-				this.log('registered attr report listener');
-			})
-			.catch(err => {
-				// Registering attr reporting failed
-				this.error('failed to register attr report listener', err);
-			});
-
-		// Humidity Cluster (0x0405)
-		this.log('Initializing Humidity (0x0405) Cluster');
-		// The minimal reporting interval in seconds (e.g. 10 (seconds)):
-		this.minIntHum = this.getSetting('minIntHum') || 60;
-		// The maximal reporting interval in seconds (e.g. 300 (seconds))
-		this.maxIntHum = this.getSetting('maxIntHum') || 180;
-		// Reportable change; the attribute should report its value when the value is changed more than this setting.
-		this.repChangeHum = this.getSetting('repChangeHum') || 100; // note: 1 = 0.01 [%]
-
-		// Register the AttributeReportListener
-		this.registerAttrReportListener(
-				'msRelativeHumidity', // Cluster
-				'measuredValue', // Attr
-				this.minIntHum,
-				this.maxIntHum,
-				this.repChangeHum,
-				this.onHumidityReport.bind(this), // Callback with value
-				0) // The endpoint index
-			.then(() => {
-				// Registering attr reporting succeeded
-				this.log('registered attr report listener');
-			})
-			.catch(err => {
-				// Registering attr reporting failed
-				this.error('failed to register attr report listener', err);
-			});
-
-		// Pressure Cluster (0x0403)
-		this.log('Initializing Pressure (0x0403) Cluster');
-
-		// The minimal reporting interval in seconds (e.g. 10 (seconds)):
-		this.minIntPres = this.getSetting('minIntPres') || 60;
-		// The maximal reporting interval in seconds (e.g. 300 (seconds))
-		this.maxIntPres = this.getSetting('maxIntPres') || 240;
-		// Reportable change; the attribute should report its value when the value is changed more than this setting.
-		this.repChangePres = this.getSetting('repChangePres') || 1; // note: 1 = 0.01 [%]
-
-		// Register the AttributeReportListener
-		this.registerAttrReportListener(
-				'msPressureMeasurement', // Cluster
-				'16', // Attr
-				this.minIntPres,
-				this.maxIntPres,
-				this.repChangePres,
-				this.onPressureReport.bind(this), // Callback with value
-				0) // The endpoint index
-			.then(() => {
-				// Registering attr reporting succeeded
-				this.log('registered attr report listener');
-			})
-			.catch(err => {
-				// Registering attr reporting failed
-				this.error('failed to register attr report listener', err);
-			});
-		this.registerCapability('measure_pressure', 'msPressureMeasurement', {
-			/*
-			getOpts: {
-				pollInterval: 10000,
-			},
-			*/
-		}, 0);
-
-		this.log('Fully initialized');
-
-	}
-	onTemperatureReport(value) {
-		let parsedValue = Math.round((value / 100) * 10) / 10;
-		this.log('msTemperatureMeasurement - measuredValue', value, parsedValue);
-		this.setCapabilityValue('measure_temperature', parsedValue);
-	}
-
-	onHumidityReport(value) {
-		let parsedValue = Math.round((value / 100) * 10) / 10;
-		this.log('msRelativeHumidity - measuredValue', value, parsedValue);
-		this.setCapabilityValue('measure_humidity', parsedValue);
-	}
-
-	onPressureReport(value) {
-		let parsedValue = Math.round((value / 100) * 10);
-		this.log('msPressureMeasurement - measuredValue', value, parsedValue);
-		this.setCapabilityValue('measure_pressure', parsedValue);
-	}
-}
-
-module.exports = AqaraWeatherSensor;
